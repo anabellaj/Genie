48cf783d37c8e9163721da067f11aae5709061b1
Andreina Rivas <andreinarivasr02@gmail.com>
2024-05-26 14:06:31 -0400

    refine code to MVVC

--- genie_app/lib/models/user.dart
+++ genie_app/lib/models/user.dart
@@ -3,26 +3,48 @@ import 'package:uuid/uuid.dart';
 const uuid = Uuid();
 
 class User {
-//  User(
-//       {String? id,
-//       required this.name,
-//       required this.lastName,
-//       required this.username,
-//       required this.university,
-//       required this.career,
-//       required this.intetests,
-//       required this.chats,
-//       required this.studyGroups})
-//       : id = id ?? uuid.v4();
-//   final String id;
-//   final String name;
-//   final String lastName;
-//   final String username;
-//   final String university;
-//   final String career;
-//   final List<String> intetests;
-//   final List<String> chats;
-//   final List<String> studyGroups;
+ User(String emailPam, String passwordPam,){
+      name= "";
+      email = emailPam;
+      password = passwordPam;
+      username="";
+      university="";
+      career="";
+    }
+      
+  late String id;
+  late String name;
+  late String email;
+  late String password;
+  late String username;
+  late String university;
+  late String career;
+  late List<String> interests;
+  late List<String> chats;
+  late List<String> studyGroups;
 
+  Map<String, dynamic> toJson()=>
+    {
+      "id":id,
+      "name":name,
+      "email":email,
+      "password":password,
+      "username":username,
+      "university":university,
+      "career":career,
+      "interests":interests.toString(),
+      "chats": chats.toString(),
+      "studyGroups":studyGroups.toString()
+
+    };
+  User.fromJson(Map<String,dynamic> json):
+    id = json['id'],
+    name = json['name'],
+    email = json['email'],
+    password = json['password'],
+    username = json['username'],
+    university = json['university'],
+    career = json['career'];    
+  
   
 }
---
+++ genie_app/lib/viewModel/controller.dart
@@ -0,0 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Controller{
+  static Future<bool>  getLoggedInUser() async{
+    final prefs = await SharedPreferences.getInstance();
+    var answer = await prefs.getBool("isLoggedIn");
+    if(answer!=null){
+      if(answer){
+       return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }
+}
